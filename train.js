18d692fca3209881acb2ce1536839811d60b82c3
Ulughdev <ulugh0618@gmail.com>
2024-02-21 17:53:32 -0800

    F- TASK

--- train.js
+++ train.js
@@ -1,11 +1,24 @@
-// E - TASK
+// // F - TASK
+// function findDoublers(string){
+//     let b = [...string];
+//     return new Set(b).size !== b.length;
+// }
 
-function getReverse(a) {
-    let b = [...a];
-    let c = b.reverse();
-    let d = c.join("");
-    return d;
+// const a = findDoublers("hello");
+// console.log(a);
+
+const animal_list = ["fox", "ant", "bird", "cat", "dog"];
+let a = [];
+
+function findAnimals(txt){
+    if(new Set(txt).size !== animal_list.length) {
+        a.push(animal_list.size)
+
+    }else {
+       return console.log("Xato")
+    }
+
 }
 
-const h = getReverse("hello");
-console.log(h);
+const javob = findAnimals("dogcat");
+console.log("javob: ", javob);
