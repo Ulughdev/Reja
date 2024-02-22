8f76a2750aa8c7880632439843b119e0a7832d15
Ulughdev <ulugh0618@gmail.com>
2024-02-21 17:55:00 -0800

    TASK - F

--- train.js
+++ train.js
@@ -1,24 +1,9 @@
-// // F - TASK
-// function findDoublers(string){
-//     let b = [...string];
-//     return new Set(b).size !== b.length;
-// }
-
-// const a = findDoublers("hello");
-// console.log(a);
-
-const animal_list = ["fox", "ant", "bird", "cat", "dog"];
-let a = [];
-
-function findAnimals(txt){
-    if(new Set(txt).size !== animal_list.length) {
-        a.push(animal_list.size)
-
-    }else {
-       return console.log("Xato")
-    }
-
+// F - TASK
+function findDoublers(string){
+    let b = [...string];
+    return new Set(b).size !== b.length;
 }
 
-const javob = findAnimals("dogcat");
-console.log("javob: ", javob);
+const a = findDoublers("hello");
+console.log(a);
+
